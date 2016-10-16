fb8ddd8a6b049efe85a81c1160ed9c4dc83bbf5f
fiber <fiber@LAB29PC09>
2016-10-16 16:19:07 +0300

    Played Around . Trying to add reflective generation.

---
+++ TestApp/Controllers/ATPController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class ATPController : Controller
+    {
+        // GET: ATP
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Box()
+        {
+            BoxModel viewModel = new BoxModel();
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult RequestBox(BoxModel viewModel)
+        {
+            if (viewModel.Width == 1) { viewModel.Width = (int)2e5; }
+
+
+            string message = string.Format("The request for box with with size {0}:{1}:{2} and weight {3}, color {4} and material {5} was successfully accepted."
+                ,viewModel.Width,viewModel.Weight,viewModel.Length,viewModel.Weight,viewModel.Color,viewModel.Material);
+            //return View(viewModel);
+            //return RedirectToAction("Box",viewModel);
+            ViewBag.SuccessMessage = message;
+            return View();
+        }
+
+    }
+}
---
+++ TestApp/Models/BoxModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.Models
+{
+    public class BoxModel
+    {
+
+        public BoxModel()
+        {
+            Color = "Shitty";
+        }
+
+        public String Color { get; set; }
+        public String Material { get; set; }
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
